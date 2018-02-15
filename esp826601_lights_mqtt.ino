da9f060fedc2c4ef91b5bbe9a4bc4dce5b47ae06
nrtingson <nrtingson@gmail.com>
2018-02-15 17:42:18 +0800

    smart classroom project code for lights

---
+++ esp826601_lights_mqtt.ino
@@ -0,0 +1,147 @@
+/*
+This sketch reads the MFRC522 RFID reader and sends out MQTT messages when cards are scanned. It also subscribes to MQTT messages telling it to open the door lock, which is actuated by a relay switching an electronic door strike.
+
+ To install the ESP8266 board, (using Arduino 1.6.4+):
+  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
+       http://arduino.esp8266.com/stable/package_esp8266com_index.json
+  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
+  - Select your ESP8266 in "Tools -> Board"
+
+
+MFRC522 pin connections
+SDA=>D2
+SCK=>D5
+MOSI=>D7
+MISO=>D6
+IRC=>NC
+RST=>D1
+
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+// Update these with values suitable for your network.
+
+
+#define RELAY_PIN 3
+
+
+
+
+const char* ssid = "SC_NIGHTOWL_LAB";
+const char* password = "a78ae1be68";
+const char* mqtt_server = "10.7.1.1";
+const char* mqtt_username = "smart_classroom";
+const char* mqtt_password = "FF4BpcMHZVb9dCBVTRBq";
+const char* mqtt_id = "NIGHTOWL_lights";
+const char* subscribe_lights = "smartclassroom/event/lights/on";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+char button_value[50];
+
+void setup() {
+  // Initialize the BUILTIN_LED pin as an output
+  pinMode(RELAY_PIN, OUTPUT);
+  
+
+  ArduinoOTA.setPort(8266);
+  ArduinoOTA.setHostname("NightOwl-Lights");
+  ArduinoOTA.setPassword((const char *)"123");
+  
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+   
+  });
+  ArduinoOTA.onEnd([]() {
+    
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+   
+  });
+  ArduinoOTA.begin();
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  reconnect();
+  digitalWrite(RELAY_PIN, HIGH);
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+ 
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+ 
+  memset(button_value, 0, sizeof(button_value));
+  strncpy(button_value, (char *)payload, length);
+
+  if(strcmp(button_value, "true")==0) {
+    digitalWrite(RELAY_PIN, HIGH);
+  }
+  else if(strcmp(button_value, "false")==0) {
+    digitalWrite(RELAY_PIN, LOW);
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    // Attempt to connect
+    if (client.connect(mqtt_id, mqtt_username, mqtt_password)) {
+      // Once connected, publish an announcement...
+      client.subscribe(subscribe_lights);
+    } else {
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+bool ota_flag = true;
+uint16_t time_elapsed = 0;
+
+void loop() {
+  if(ota_flag)
+  {
+    while(time_elapsed < 15000)
+    {
+      ArduinoOTA.handle();
+      time_elapsed = millis();
+      delay(10);
+    }
+    ota_flag = false;
+  }
+  delay(500);
+
+    client.loop();
+ 
+}
+
+
+

ce0827e405d4a00d90697e6b0a05cb4824678d5c
Dolf Andringa <dolfandringa@gmail.com>
2019-03-16 11:59:12 +0800

    Modified door code to work with smart_classroom library

--- sc_door/sc_door.ino
+++ sc_door/sc_door.ino
@@ -1,168 +1,62 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <ESP8266httpUpdate.h>
-#include <PubSubClient.h>
+#include <smart_classroom.h>
 #include <SPI.h>
-#include <Ticker.h>
-#include <FS.h>
-#include <ArduinoJson.h>
 #include <MFRC522.h>  // Library for Mifare RC522 Devices
  
 Ticker unlocker;
-Ticker blinker;
-
 
 #define RST_PIN D1  
 #define SS_PIN  D2 
 
 #define BUZZER_PIN D3 //buzzer
-#define LED_PIN2 D4
 #define TAGSIZE 12
 #define RELAY_PIN 10
 #define LED_PIN LED_BUILTIN
+#define LED2_PIN D4
 
 #define SWITCH_PIN_MODE INPUT
 #define SWITCH_PIN_DETECT_EDGE RISING
 #define SWITCH_PIN D8
 
-
-
-
-
 uint8_t successRead; //variable integer to keep if we hace successful read
-
-uint32_t numblink;
-uint32_t maxblink;
 
 byte readCard[8]; //Stores scanned ID
 char temp[3];
 char cardID[9];
-char c;
 
 const int switch_interrupt = digitalPinToInterrupt(SWITCH_PIN);
-
-struct Settings{
-  char ssid[64];
-  char password[64];
-  char mqtt_server[15];
-  char mqtt_username[32];
-  char mqtt_password[32];
-  char mqtt_id[32];
-};
 
 const char* cardread_topic = "smartclassroom/Door/cardread";
 const char* lock_topic = "smartclassroom/Door/open";
 const char* door_open_topic = "smartclassroom/Door/open";
 const char* door_open_announce_topic = "smartclassroom/Door/announce/open";
-const char* subscribe_update_topic = "smartclassroom/Door/update";
+const char* update_topic = "smartclassroom/Door/update";
 
-uint8_t tags;
-
-Settings settings;
-WiFiClient espClient;
-PubSubClient client(espClient);
+SmartClassroom sc;
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 MFRC522::MIFARE_Key key;
 
-char button_value[50];
+void sub_func(){
+    sc.mqtt.subscribe(lock_topic);
+    sc.mqtt.subscribe(door_open_topic);
+    sc.mqtt.subscribe(update_topic);
+}
+
+char payload_value[50];
 
 void setup() {
   pinMode(BUZZER_PIN, OUTPUT);
-  pinMode(LED_PIN2, OUTPUT);
   pinMode(RELAY_PIN, OUTPUT);
-  pinMode(LED_PIN, OUTPUT);
   pinMode(SWITCH_PIN, SWITCH_PIN_MODE);
-  digitalWrite(LED_PIN, HIGH);
-  digitalWrite(LED_PIN2, HIGH);
   lock();
   Serial.begin(115200);
-  SPIFFS.begin();
-  read_settings();
   SPI.begin();
-
   mfrc522.PCD_Init(); //Initialize MFRC522 hardware
-  delay(250);
-  setup_wifi();
-  Serial.print("MQTT Server ");
-  Serial.println(settings.mqtt_server);
-  client.setServer(settings.mqtt_server, 1883);
-  client.setCallback(callback);
-  reconnect();
+  sc.begin(LED_PIN, LED2_PIN);
   //ShowReaderDetails();
-  client.publish(door_open_announce_topic, "false");
+  sc.mqtt.publish(door_open_announce_topic, "false");
   Serial.println(F("-------------------"));
   Serial.println(F("Everything Ready"));
   Serial.println(("Waiting PICCs to be scanned"));
-}
-
-
-void read_settings() {
-  File f = SPIFFS.open("/settings.json", "r");
-  Serial.println("");
-  Serial.println("Opening /settings.json");
-  if(!f){
-    Serial.println("Failed to read file");
-    return;
-  }
-  const size_t capacity = JSON_OBJECT_SIZE(6) + 160;
-  DynamicJsonBuffer jsonBuffer(capacity);
-  JsonObject &root = jsonBuffer.parseObject(f);
-  if(!root.success()){
-    Serial.println("Failed to read file");
-    return;
-  }
-  f.close();
-  Serial.println("Finished parsing settings file");
-  strlcpy(settings.ssid, root["ssid"], sizeof(settings.ssid));
-  strlcpy(settings.password, root["password"], sizeof(settings.password));
-  strlcpy(settings.mqtt_server, root["mqtt_server"], sizeof(settings.mqtt_server));
-  strlcpy(settings.mqtt_username, root["mqtt_username"], sizeof(settings.mqtt_username));
-  strlcpy(settings.mqtt_password, root["mqtt_password"], sizeof(settings.mqtt_password));
-  strlcpy(settings.mqtt_id, root["mqtt_id"], sizeof(settings.mqtt_id));
-}
-
-void blink(uint32_t num_blink, uint16_t delay_time) {
-  //blink num_blink times with delay_time interval
-  maxblink = num_blink;
-  numblink = 0;
-  blinker.attach_ms(delay_time, switch_led);
-}
-
-void switch_led() {
-  //turn the LED off if its on, or on if its off, as long as we should be blinking
-  uint8_t led_state = digitalRead(LED_PIN);
-  digitalWrite(LED_PIN, !led_state);
-  if (led_state == LOW) {
-    //LED was on, now turned off again, advance num_blink;
-    numblink++;
-  }
-  if (numblink >= maxblink) {
-    //stop blinking because we blinked the maximum number
-    blinker.detach();
-  }
-}
-
-void setup_wifi() {
-
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(settings.ssid);
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(settings.ssid, settings.password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    blink(5, 100);
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -175,85 +69,39 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
   Serial.println();
 
-  blink(4, 200);
+  sc.blink(4, 200);
 
-  if(strcmp(topic, subscribe_update_topic) == 0){
-    checkUpdate((char *)payload);
+  if(strcmp(topic, update_topic) == 0){
+    sc.checkUpdate((char *)payload);
     return;
   }
   
-  memset(button_value, 0, sizeof(button_value));
-  strncpy(button_value, (char *)payload, length);
+  memset(payload_value, 0, sizeof(payload_value));
+  strncpy(payload_value, (char *)payload, length);
 
-  if (strcmp(button_value, "true") == 0) {
+  if (strcmp(payload_value, "true") == 0) {
     //digitalWrite(LED_PIN, HIGH);
     unlock(5000);
   }
-  else if (strcmp(button_value, "false") == 0) {
+  else if (strcmp(payload_value, "false") == 0) {
     //digitalWrite(LED_PIN, LOW);
     lock();
-  }
-}
-
-void checkUpdate(char* update_uri) {
-  Serial.print("Checking for update on url ");
-  Serial.println(update_uri);
-  ESPhttpUpdate.setLedPin(LED_PIN, LOW);
-  t_httpUpdate_return ret = ESPhttpUpdate.update(update_uri);
-  switch (ret) {
-      case HTTP_UPDATE_FAILED:
-        Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
-        break;
-
-      case HTTP_UPDATE_NO_UPDATES:
-        Serial.println("HTTP_UPDATE_NO_UPDATES");
-        break;
-
-      case HTTP_UPDATE_OK:
-        Serial.println("HTTP_UPDATE_OK");
-        break;
-    }
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection with username ");
-    Serial.print(settings.mqtt_username);
-    Serial.print(" and password ");
-    Serial.print(settings.mqtt_password);
-    Serial.print("...");
-    // Attempt to connect
-    if (client.connect(settings.mqtt_id, settings.mqtt_username, settings.mqtt_password)) {
-      Serial.println("connected");
-      client.subscribe(lock_topic);
-      client.subscribe(door_open_topic);
-      client.subscribe(subscribe_update_topic);
-      blink(3,200);
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      blink(50000, 100); //Blink fast in the mean time with 100ms intervals
-      delay(5000);
-    }
   }
 }
 
 void loop() {
   
   do {
-    if (!client.connected()) {
-      reconnect();
+    if (!sc.mqtt.connected()) {
+      sc.reconnect(sub_func);
     }
-    client.loop();
+    sc.mqtt.loop();
     successRead = getID();
   }
   while (!successRead);
   Serial.println("");
   Serial.println("Publishing: ");
-  client.publish(cardread_topic, cardID);
+  sc.mqtt.publish(cardread_topic, cardID);
   Serial.println(cardID);
 }
 
@@ -309,13 +157,13 @@ void unlock_button() {
   //called when the button to unlock the door is pressed through an interrupt
   detachInterrupt(switch_interrupt); //Make sure the interrupt isn't triggered as long as the door is open.
   Serial.println("Button pressed");
-  client.publish(door_open_announce_topic, "true");
+  sc.mqtt.publish(door_open_announce_topic, "true");
   unlock(5000);
 }
 
 void unlock(int unlock_time) {
   //unlock the door for unlock_time milliseconds. If unlock_time <=0, stay open
-  blink(2,200);
+  sc.blink(2,200);
   Serial.println("Unlocking");
   if(digitalRead(RELAY_PIN) == HIGH){
     digitalWrite(RELAY_PIN, LOW);
@@ -335,13 +183,13 @@ void unlock(int unlock_time) {
 void auto_lock(){
   //called when the door is locked automatically by the code
   unlocker.detach();
-  client.publish(door_open_announce_topic, "false");
+  sc.mqtt.publish(door_open_announce_topic, "false");
   lock();
 }
 
 void lock() { 
   //lock the door
-  blink(2, 200);
+  sc.blink(2, 200);
   Serial.println("Locking");
   attachInterrupt(switch_interrupt, unlock_button, SWITCH_PIN_DETECT_EDGE); //attach the interrupt again to listen for the unlock button press
   if(digitalRead(RELAY_PIN) == LOW){
